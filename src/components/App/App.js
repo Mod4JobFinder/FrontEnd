462c776d61f621b7db306f43b1089259d342ed48
Aaron Foucheaux <afoucheaux@gmail.com>
2021-05-13 17:28:35 -0500

    Router added to app and index.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Landing from '../Landing/Landing.js';
+import JobsView from '../JobsView/JobsView.js';
+import UserView from '../UserView/UserView.js';
 
 function App() {
   return (
-
+    <Switch className='app'>
+      
+    </Switch>
   );
 }
 
---
+++ src/components/UserView/UserView.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function UserView() {
+  return (
+    <p>Test 3</p>
+  );
+}
+
+export default UserView;
