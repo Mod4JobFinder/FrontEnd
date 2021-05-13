d3ae85dfa84b483675ee8d24a29149b90667570b
Aaron Foucheaux <afoucheaux@gmail.com>
2021-05-13 17:04:26 -0500

    Cypress fixtures setup

---
+++ cypress/fixtures/mockJobListData.js
@@ -0,0 +1,43 @@
+{
+  "data": [{
+    "id": 1,
+    "type": "Part Time",
+    "attributes": { "title": "Software1", "company": "Google", "category": ["ALL", 'The', 'Tags'], "description": "Are to a dev?", "location": "Anywhere", "publish_date": "datetime", "link": "https://github.com/orgs/Mod4JobFinder/projects/1" }
+  }, {
+    "id": 2,
+    "type": "Part Time",
+    "attributes": { "title": "Software2", "company": "Google", "category": ["ALL", 'The', 'Tags'], "description": "Are to a dev?", "location": "Anywhere", "publish_date": "datetime", "link": "https://github.com/orgs/Mod4JobFinder/projects/1" }
+  }, {
+    "id": 3,
+    "type": "Part Time",
+    "attributes": { "title": "Software3", "company": "Google", "category": ["ALL", 'The', 'Tags'], "description": "Are to a dev?", "location": "Anywhere", "publish_date": "datetime", "link": "https://github.com/orgs/Mod4JobFinder/projects/1" }
+  }, {
+    "id": 4,
+    "type": "Full Time",
+    "attributes": { "title": "Software4", "company": "Google", "category": ["ALL", 'The', 'Tags'], "description": "Are to a dev?", "location": "Anywhere", "publish_date": "datetime", "link": "https://github.com/orgs/Mod4JobFinder/projects/1" }
+  }, {
+    "id": 5,
+    "type": "Full Time",
+    "attributes": { "title": "Software5", "company": "Google", "category": ["ALL", 'The', 'Tags'], "description": "Are to a dev?", "location": "Anywhere", "publish_date": "datetime", "link": "https://github.com/orgs/Mod4JobFinder/projects/1" }
+  }, {
+    "id": 6,
+    "type": "Full Time",
+    "attributes": { "title": "Software6", "company": "Google", "category": ["ALL", 'The', 'Tags'], "description": "Are to a dev?", "location": "Anywhere", "publish_date": "datetime", "link": "https://github.com/orgs/Mod4JobFinder/projects/1" }
+  }, {
+    "id": 7,
+    "type": "Full Time",
+    "attributes": { "title": "Software7", "company": "Google", "category": ["ALL", 'The', 'Tags'], "description": "Are to a dev?", "location": "Anywhere", "publish_date": "datetime", "link": "https://github.com/orgs/Mod4JobFinder/projects/1" }
+  }, {
+    "id": 8,
+    "type": "Full Time",
+    "attributes": { "title": "Software8", "company": "Google", "category": ["ALL", 'The', 'Tags'], "description": "Are to a dev?", "location": "Anywhere", "publish_date": "datetime", "link": "https://github.com/orgs/Mod4JobFinder/projects/1" }
+  }, {
+    "id": 9,
+    "type": "Full Time",
+    "attributes": { "title": "Software9", "company": "Google", "category": ["ALL", 'The', 'Tags'], "description": "Are to a dev?", "location": "Anywhere", "publish_date": "datetime", "link": "https://github.com/orgs/Mod4JobFinder/projects/1" }
+  }, {
+    "id": 10,
+    "type": "Full Time",
+    "attributes": { "title": "Software10", "company": "Google", "category": ["ALL", 'The', 'Tags'], "description": "Are to a dev?", "location": "Anywhere", "publish_date": "datetime", "link": "https://github.com/orgs/Mod4JobFinder/projects/1" }
+  }]
+}
