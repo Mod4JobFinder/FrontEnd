0ea60c3a1dcd4c4effff1334ccfe4c777687ccc9
Aaron Foucheaux <afoucheaux@gmail.com>
2021-05-13 17:04:26 -0500

    Cypress fixtures setup

---
+++ cypress/fixtures/MockSalData.js
@@ -0,0 +1,13 @@
+export const mockSalaryData = {
+  'data': {
+    'id': null,
+    'type': 'Test Name Of Area'
+    'attributes': [
+      {'id': 1, 'title': 'One', 'min_salary': 1000.50, 'max_salary': 10000},
+      {'id': 2, 'title': 'Two', 'min_salary': 2000, 'max_salary': 20000.55},
+      {'id': 3, 'title': 'Three', 'min_salary': 3000, 'max_salary': 30000},
+      {'id': 4, 'title': 'Four', 'min_salary': 4000, 'max_salary': 40000},
+      {'id': 5, 'title': 'Five', 'min_salary': 5000, 'max_salary': 50000}
+    ]
+  }
+}
---
+++ cypress/fixtures/MockUserData.js
@@ -0,0 +1,19 @@
+{
+  'data': {
+    'id': null,
+    'type': 'UserOne'
+    'attributes': {
+      'first_name': 'First Name',
+      'last_name': 'Last Name',
+      'email': 'Test@Test.com',
+      'city': 'Denver',
+      'state': 'CO',
+      'zipcode': 12345,
+      'saved_jobs': [
+        { 'job_title': 'THE JOB', 'location': 'Denver', 'company': 'Google', 'url': 'https://github.com/orgs/Mod4JobFinder/projects/1'},
+        { 'job_title': 'THE JOB2', 'location': 'New York', 'company': 'Google', 'url': 'https://github.com/orgs/Mod4JobFinder/projects/1'},
+        { 'job_title': 'THE JOB3', 'location': 'Los Angeles', 'company': 'Google', 'url': 'https://github.com/orgs/Mod4JobFinder/projects/1'}
+      ]
+    }
+  }
+}
