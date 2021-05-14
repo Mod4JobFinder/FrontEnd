53fa62a7a0e3cc8e269e0993dced05276f74bb17
Aaron Foucheaux <afoucheaux@gmail.com>
2021-05-14 07:14:14 -0500

    POST new user api call

---
+++ src/apiCalls.js
@@ -0,0 +1,14 @@
+const salare = 'https://findjob-backend.herokuapp.com/api/v1/salaries?city=denver'
+const usersUrl = 'https://findjob-backend.herokuapp.com/api/v1/users'
+const loginUrl = 'https://findjob-backend.herokuapp.com/api/v1/sessions'
+
+export const postNewUser = (newUser) => {
+  return fetch(usersUrl), {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.string(newUser)
+  })
+  .then(response => response.json())
+}
