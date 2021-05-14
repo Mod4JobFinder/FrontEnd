636fa1b5d5fe6855938fe5d706ed33db3dab4389
Aaron Foucheaux <afoucheaux@gmail.com>
2021-05-14 09:49:49 -0500

    added mode button state to header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function Header() {
+
+  const [mode, setMode] = useState('light');
+
+  const handleModeChange = (e) => {
+    if (mode === 'light') {
+      setMode('dark');
+    } else {
+      setMode('light');
+    }
+  }
+
   return (
     <section className='headerLayout' data-cy='headerLayout'>
-      <button className='mode' data-cy='mode'>mode</button>
+      <button onClick={e => handleModeChange(e)} className='mode' data-cy='mode'>mode</button>
       <article className='titleBox' data-cy='titleBox'>
         <h1 className='appTitle' data-cy='appTitle'>Job Finder!<h1>
       </article>
     </section>
   )
 }
+
+export default Header;
