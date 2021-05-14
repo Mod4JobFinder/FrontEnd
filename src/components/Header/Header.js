dae5a40cbe78c0b4afd3cb1b1ebe04ef250538c9
Aaron Foucheaux <afoucheaux@gmail.com>
2021-05-14 07:45:39 -0500

    fix typo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,7 @@ function Header() {
     <section className='headerLayout' data-cy='headerLayout'>
       <button className='mode' data-cy='mode'>mode</button>
       <article className='titleBox' data-cy='titleBox'>
-        <h1 className='appTitle' data-cy'appTitle'>Job Finder!<h1>
+        <h1 className='appTitle' data-cy='appTitle'>Job Finder!<h1>
       </article>
     </section>
   )
