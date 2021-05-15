b14722c4c9732057b37831cdd1f26fa32403d167
Aaron Foucheaux <afoucheaux@gmail.com>
2021-05-15 09:27:44 -0500

    Added login to landing page update both components with Theme context

--- src/components/LoginNewForm/LoginNewForm.js
+++ src/components/LoginNewForm/LoginNewForm.js
@@ -1,8 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useCont } from 'react';
+import {ThemeContext} from '../../Context/ThemeContext';
+
 
 function LoginForm() {
+  const { color, handleModeChange } = useContext(ThemeContext)
   const [emailInput, setEmailInput] = useState('')
   const [passInput, setPassInput] = useState('')
+  const [newUser, setNewUser] = useState('hidden');
 
   const handleChange = (event, formType) => {
     if (formType === 'email') {
@@ -13,23 +17,28 @@ function LoginForm() {
   }
 
   return (
-    <section className='login' data-cy='login'>
-      <input
-        className='login-input'
-        name='email'
-        value={emailInput}
-        onChange={event => handleChange(event, 'email')}
-        type='text'
-        aria-label='User email address'
-      />
-      <input
-        className='login-input'
-        name='password'
-        value={passInput}
-        onChange={event => handleChange(event, 'password')}
-        type='password'
-        aria-label='user password input'
-      />
-    </section>
+    <main className={`mainLayout ${color}`}>
+      <section className='login' data-cy='login'>
+        <input
+          className='login-input'
+          name='email'
+          value={emailInput}
+          onChange={event => handleChange(event, 'email')}
+          type='text'
+          aria-label='User email address'
+        />
+        <input
+          className='login-input'
+          name='password'
+          value={passInput}
+          onChange={event => handleChange(event, 'password')}
+          type='password'
+          aria-label='user password input'
+        />
+      </section>
+    </main>
   )
 }
+
+
+export default LoginForm
