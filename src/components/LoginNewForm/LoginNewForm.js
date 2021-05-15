47dfcafe4ee7922b9819c09e69bad09a6f7326b7
Connor Anderson Larson <Connorandersonlarson@gmail.com>
2021-05-15 07:57:04 -0500

    Add username/password inputs to form

--- src/components/LoginNewForm/LoginNewForm.js
+++ src/components/LoginNewForm/LoginNewForm.js
@@ -1,8 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function LoginForm() {
+  const [emailInput, setEmailInput] = useState('')
+  const [passInput, setPassInput] = useState('')
+
+  const handleChange = (event, formType) => {
+    if (formType === 'email') {
+      setEmailInput(event.target.value)
+    } else if (formType === 'password') {
+      setPassInput(event.target.value)
+    }
+  }
+
   return (
     <section className='login' data-cy='login'>
+      <input
+        className='login-input'
+        name='email'
+        value={emailInput}
+        onChange={event => handleChange(event, 'email')}
+        type='text'
+        aria-label='User email address'
+      />
+      <input
+        className='login-input'
+        name='password'
+        value={passInput}
+        onChange={event => handleChange(event, 'password')}
+        type='password'
+        aria-label='user password input'
+      />
     </section>
   )
 }
