7589f425ef9aeec77376ba5a4ef258fcdba25f90
Aaron Foucheaux <afoucheaux@gmail.com>
2021-05-15 12:35:45 -0500

    Added new user form with toggle

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,12 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import './Landing.css';
 import Header from '../Header/Header.js';
 import LoginForm from '../LoginNewForm/LoginNewForm.js';
+import {ThemeContext} from '../../Context/ThemeContext';
 
 
 function Landing() {
+  const { color, handleModeChange } = useContext(ThemeContext)
+
   return (
-    <main className='LandingLayout'>
+    <main className={`LandingLayout ${color}`}>
       <Header />
       <LoginForm />
     </main>
