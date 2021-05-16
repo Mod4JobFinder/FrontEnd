932985a3f6c4f015219c07c15e1649a5cb7c6ab2
Aaron Foucheaux <afoucheaux@gmail.com>
2021-05-15 19:55:58 -0500

    UserContext added and in needed components. Original state is committed out.

---
+++ src/Context/UserContext.js
@@ -0,0 +1,63 @@
+import React, { createContext, useState } from 'react';
+
+export const UserContext = createContext();
+
+export const UserContextProvider = (props) => {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [city, setCity] = useState('');
+  const [state, setState] = useState('');
+  const [zipcode, setZipcode] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordConfirmation, setPasswordConfirmation] = useState('');
+  const [userSavedJobs, setUserSavedJobs] = useState([]);
+
+  const handleUserChange = (event, formType) => {
+    if (formType === 'firstName') {
+      setFirstName(event.target.value)
+    } else if (formType === 'lastName') {
+      setLastName(event.target.value)
+    } else if (formType === 'city') {
+      setCity(event.target.value)
+    } else if (formType === 'state') {
+      setState(event.target.value)
+    } else if (formType === 'zipcode') {
+      setZipcode(event.target.value)
+    } else if (formType === 'email') {
+      setEmail(event.target.value)
+    } else if (formType === 'password') {
+      setPassword(event.target.value)
+    } else if (formType === 'passwordConfirmation') {
+      setPasswordConfirmation(event.target.value)
+    }
+  }
+
+  const clearUserForm = () => {
+    setFirstName('')
+    setLastName('')
+    setCity('')
+    setState('')
+    setZipcode('')
+    setEmail('')
+    setPassword('')
+    setPasswordConfirmation('')
+  }
+
+  return (
+    <UserContext.Provider value={
+      {firstName,
+      lastName,
+      email,
+      city,
+      state,
+      zipcode,
+      password,
+      passwordConfirmation,
+      handleUserChange,
+      clearUserForm}
+     }>
+      {props.children}
+    </UserContext.Provider>
+  )
+}
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,20 +3,23 @@ import { Route, Switch } from 'react-router-dom';
 import Landing from '../Landing/Landing.js';
 import JobsView from '../JobsView/JobsView.js';
 import UserView from '../UserView/UserView.js';
-import {ThemeContextProvider} from '../../Context/ThemeContext';
 import NewUserScreen from '../NewUserScreen/NewUserScreen.js';
+import {ThemeContextProvider} from '../../Context/ThemeContext';
+import {UserContextProvider} from '../../Context/UserContext';
 
 function App() {
 
   return (
-    <ThemeContextProvider>
-      <Switch className='app'>
-        <Route exact path='/' component={ Landing }/>
-        <Route exact path='/NewUser' component={ NewUserScreen }/>
-        <Route exact path='/JobsView' component={ JobsView }/>
-        <Route exact path='/UserView' component={ UserView }/>
-      </Switch>
-    </ThemeContextProvider>
+    <UserContextProvider>
+      <ThemeContextProvider>
+        <Switch className='app'>
+          <Route exact path='/' component={ Landing }/>
+          <Route exact path='/NewUser' component={ NewUserScreen }/>
+          <Route exact path='/JobsView' component={ JobsView }/>
+          <Route exact path='/UserView' component={ UserView }/>
+        </Switch>
+      </ThemeContextProvider>
+    </UserContextProvider>
   );
 }
 
