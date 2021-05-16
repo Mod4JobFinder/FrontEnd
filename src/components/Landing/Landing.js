bdad15c8c85f0c34bf451244e2cc73e41c57ee7a
Aaron Foucheaux <afoucheaux@gmail.com>
2021-05-16 16:56:07 -0500

    update to yml file for Travis bug fix

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -4,7 +4,6 @@ import Header from '../Header/Header.js';
 import LoginForm from '../LoginForm/LoginForm.js';
 import {ThemeContext} from '../../Context/ThemeContext';
 
-
 function Landing() {
   const { color, handleModeChange } = useContext(ThemeContext)
 
