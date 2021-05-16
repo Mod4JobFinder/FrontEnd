494f2246665a057ae3439f2b071e2625acc7ee83
Aaron Foucheaux <afoucheaux@gmail.com>
2021-05-16 16:56:07 -0500

    update to yml file for Travis bug fix

--- src/components/JobsView/JobsView.js
+++ src/components/JobsView/JobsView.js
@@ -1,11 +1,13 @@
 import React, { useContext } from 'react';
 import './JobsView.css';
 import {ThemeContext} from '../../Context/ThemeContext';
+import Header from '../Header/Header.js'
 
 function JobsView() {
   const { color } = useContext(ThemeContext)
   return (
     <section className='jobViewLayout' data-cy='jobViewLayout'>
+      <Header />
       <p>Test 2</p>
     </section>
   );
