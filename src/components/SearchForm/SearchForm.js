dbc190baf9b2cce95c7bfd9e842fcce381d9bd42
Aaron Foucheaux <afoucheaux@gmail.com>
2021-05-19 08:44:31 -0500

    Added job filter buttons and city state inputs

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -10,7 +10,36 @@ function SearchForm() {
 
   return (
     <section>
-      <p>This is a search form</p>
+      <article className='jobFilter' data-cy='jobFilter'>
+        <button className='dataAnalyst' data-cy='dataAnalyst'>Data Analyst</button>
+        <button className='dataScientist' data-cy='dataScientist'>Data Scientist</button>
+        <button className='mobileDev' data-cy='mobileDev'>Mobile Developer</button>
+        <button className='qaEngineer' data-cy='qaEngineer'>QA Engineer</button>
+        <button className='sysAdmin' data-cy='sysAdmin'>Systems Admin</button>
+        <button className='webDev' data-cy='webDev'>Web Developer</button>
+      </article>
+      <article>
+        <input
+          data-cy='city'
+          className='cityInput'
+          name='city'
+          value={city}
+          onChange={event => setEmail(event.target.value)}
+          type='text'
+          aria-label='city to search'
+          placeholder='City'
+        />
+        <input
+          data-cy='state'
+          className='stateInput'
+          name='state'
+          value={state}
+          onChange={event => setPassword(event.target.value)}
+          type='state'
+          aria-label='state to search'
+          placeholder='State'
+        />
+      </article>
     </section>
   )
 }
