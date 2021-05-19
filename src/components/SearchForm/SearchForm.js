aadb7da5824119f9a4195e68749d400729c65224
Aaron Foucheaux <afoucheaux@gmail.com>
2021-05-19 11:49:30 -0500

    search and clear connected

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -7,15 +7,29 @@ function SearchForm() {
   const [city, setCity] = useState('');
   const [jobTitle, setJobTitle]= useState('');
 
+  const handleJobButton = (e, job) => {
+    if (job === jobTitle) {
+      setJobTitle('');
+    } else {
+      setJobTitle(job);
+    }
+  }
+
+  const clearSearch = () => {
+    setJobTitle('');
+    setCity('');
+  }
+
   return (
     <section>
+      <p>{`Looking for ${jobTitle} in ${city}.`}</p>
       <article className='jobFilter' data-cy='jobFilter'>
-        <button className='dataAnalyst' data-cy='dataAnalyst'>Data Analyst</button>
-        <button className='dataScientist' data-cy='dataScientist'>Data Scientist</button>
-        <button className='mobileDev' data-cy='mobileDev'>Mobile Developer</button>
-        <button className='qaEngineer' data-cy='qaEngineer'>QA Engineer</button>
-        <button className='sysAdmin' data-cy='sysAdmin'>Systems Admin</button>
-        <button className='webDev' data-cy='webDev'>Web Developer</button>
+        <button className='dataAnalyst' data-cy='dataAnalyst' onClick={e => handleJobButton(e, 'Data Analyst')}>Data Analyst</button>
+        <button className='dataScientist' data-cy='dataScientist' onClick={e => handleJobButton(e, 'Data Scientist')}>Data Scientist</button>
+        <button className='mobileDev' data-cy='mobileDev' onClick={e => handleJobButton(e, 'Mobile Developer')}>Mobile Developer</button>
+        <button className='qaEngineer' data-cy='qaEngineer' onClick={e => handleJobButton(e, 'QA Engineer')}>QA Engineer</button>
+        <button className='sysAdmin' data-cy='sysAdmin' onClick={e => handleJobButton(e, 'Systems Administrator')}>Systems Admin</button>
+        <button className='webDev' data-cy='webDev' onClick={e => handleJobButton(e, 'Web Developer')}>Web Developer</button>
       </article>
       <article>
         <input
@@ -23,10 +37,13 @@ function SearchForm() {
           className='cityInput'
           name='city'
           value={city}
+          onChange={event => setCity(event.target.value)}
           type='text'
           aria-label='city to search'
           placeholder='City'
         />
+        <button className='submitSearch' data-cy='submitSearch'>Submit</button>
+        <button className='clearSearch' data-cy='clearSearch' onClick={clearSearch}>Clear Search</button>
       </article>
     </section>
   )
