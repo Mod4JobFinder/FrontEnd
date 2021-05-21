15d0a8456082cb2e4d5c7e7b8c501e226621e2f9
Aaron Foucheaux <afoucheaux@gmail.com>
2021-05-20 19:45:09 -0500

    save cards update

--- src/components/SaveCard/SaveCard.js
+++ src/components/SaveCard/SaveCard.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './SaveCard.js';
 
-function SaveCard() {
+function SaveCard({ title,company , location, id, updateDetailJob }) {
   return (
-    <p>A saved card</p>
+    <button className="savedCard" data-cy='savedCard' id={id} onClick={e => updateDetailJob(id)}>
+      <p className='savTitle' data-cy='savTitle'>{title}</p>
+      <p className='savCompany' data-cy='savCompany'>{company}</p>
+      <p className='savLocal' data-cy='savLocal'>{location}</p>
+    </button>
   )
 }
 
