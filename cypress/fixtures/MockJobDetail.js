ac086f649ed706fbf5e7710396d92980775f6b66
Aaron Foucheaux <afoucheaux@gmail.com>
2021-05-21 08:57:15 -0500

    Stubbed postJobToUser and fixture added to testing.

---
+++ cypress/fixtures/MockJobDetail.js
@@ -0,0 +1,7 @@
+{ 
+  data: {
+   "id": 2,
+   "type": "Part Time",
+   "attributes": { "title": "Software2", "company": "Google", "category": ["ALL", 'The', 'Tags'], "description": "Are to a dev?", "location": "Anywhere", "publish_date": "datetime", "link": "https://github.com/orgs/Mod4JobFinder/projects/1" }
+ }
+}
