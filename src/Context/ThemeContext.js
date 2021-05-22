f0dd251d160966b75da0a01894f5d38d0f0a65f9
Aaron Foucheaux <afoucheaux@gmail.com>
2021-05-22 11:25:10 -0500

    light dark mode set up.

--- src/Context/ThemeContext.js
+++ src/Context/ThemeContext.js
@@ -4,13 +4,62 @@ export const ThemeContext = createContext();
 
 export const ThemeContextProvider = (props) => {
 
-  const [color, setColor] = useState('dark');
+    const lightMode = {
+      blue: {
+        backgroundColor: 'darkblue',
+        color: 'white',
+      },
+      pink: {
+        backgroundColor: 'violet',
+        color: 'white',
+      },
+      green: {
+        backgroundColor: 'mediumseagreen',
+        color: 'black',
+      },
+      orange: {
+        backgroundColor: 'darkorange',
+        color: 'black',
+      },
+      pageBackGround: {
+        backgroundColor: 'white',
+        color: 'black',
+      }
+    }
+
+    const darkMode = {
+      blue: {
+        backgroundColor: 'powderblue',
+        color: 'black',
+      },
+      pink: {
+        backgroundColor: 'plum',
+        color: 'black',
+      },
+      green: {
+        backgroundColor: 'springgreen',
+        color: 'black',
+      },
+      orange: {
+        backgroundColor: 'orange',
+        color: 'black',
+      },
+      pageBackGround: {
+        backgroundColor: 'black',
+        color: 'white',
+      }
+    }
+
+  const [mode, setMode] = useState('dark')
+  const [color, setColor] = useState(darkMode);
 
   const handleModeChange = (e) => {
-    if (color === 'light') {
-      setColor('dark');
+    if (mode === 'light') {
+      setMode('dark')
+      setColor(darkMode);
     } else {
-      setColor('light');
+      setMode('light')
+      setColor(lightMode);
     }
   }
 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,7 +7,7 @@ function Header( {hidden} ) {
   const { color, handleModeChange } = useContext(ThemeContext)
 
   return (
-    <section className={`headerLayout ${color}`} data-cy='headerLayout'>
+    <section className='headerLayout' style={color.blue} data-cy='headerLayout'>
       <button onClick={e => handleModeChange(e)} className='mode' data-cy='mode'>mode</button>
       <article className='titleBox' data-cy='titleBox'>
         <h1 className='appTitle' data-cy='appTitle'>Job Finder!</h1>
