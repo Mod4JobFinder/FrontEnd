ae07a5483c5cd66407bf4ce1bbc5b27227128f7c
Connor Anderson Larson <Connorandersonlarson@gmail.com>
2021-05-23 13:59:46 -0500

    Add styling to apply/delete buttons

--- src/components/JobDetail/JobDetail.js
+++ src/components/JobDetail/JobDetail.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { ThemeContext } from '../../Context/ThemeContext';
 import './JobDetail.css';
 
 function JobDetail( {title, company, location, description, url, deleteJob, id } ) {
+  const { color } = useContext(ThemeContext)
+
   return (
     <article className='detailSec' data-cy='detailSec'>
       <h1 className='detTitle' data-cy='detTitle'>{title}</h1>
@@ -9,8 +12,10 @@ function JobDetail( {title, company, location, description, url, deleteJob, id }
         <p className='detComp' data-cy='detComp'>{company}</p>
         <p className='detLocal' data-cy='detLocal'>{location}</p>
         <p className='detDesc' data-cy='detDesc'>{description}</p>
-        <a href={url} className='detUrl' data-cy='detUrl'>Apply Here</a>
-        <button className='deleteJobButton' data-cy='deleteJobButton' id={id} onClick={e => deleteJob(id)}>delete job</button>
+        <div className='appDel'>
+          <a href={url} className='detUrl' style={color.green} data-cy='detUrl'>Apply Here</a>
+          <button className='deleteJobButton' style={color.pink} data-cy='deleteJobButton' id={id} onClick={e => deleteJob(id)}>Delete Job</button>
+        </div>
       </div>
     </article>
   )
