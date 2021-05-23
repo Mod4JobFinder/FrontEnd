4810dac3bc98f0608f40d4205472446ce422d4a6
Connor Anderson Larson <Connorandersonlarson@gmail.com>
2021-05-23 07:55:34 -0500

    fix merge conflicts

--- src/components/UserView/UserView.js
+++ src/components/UserView/UserView.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useContext }from 'react';
 import './UserView.css';
 import Header from '../Header/Header.js';
 import SaveCard from '../SaveCard/SaveCard.js';
 import JobDetail from '../JobDetail/JobDetail.js';
+import {ThemeContext} from '../../Context/ThemeContext';
+import {UserContext} from '../../Context/UserContext';
 
 function UserView() {
+  const {currentUser ,saveJob, setUserSavedJobs} = useContext(UserContext);
+
   return (
     <>
       <Header />
