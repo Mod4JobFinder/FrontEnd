ce1e1a6d4ee7967ded7c463430234620941e9648
Connor Anderson Larson <Connorandersonlarson@gmail.com>
2021-05-24 08:13:16 -0500

    Add hover effect to saved job cards

--- src/components/SaveCard/SaveCard.js
+++ src/components/SaveCard/SaveCard.js
@@ -7,9 +7,11 @@ function SaveCard({ title,company , location, id, updateDetailJob }) {
 
   return (
     <button className="savedCard" data-cy='savedCard' id={id} style={color.orangeborder} onClick={e => updateDetailJob(id)}>
+      <div className="bd">
       <p className='savTitle' data-cy='savTitle'>{title}</p>
       <p className='savCompany' data-cy='savCompany'>{company}</p>
       <p className='savLocal' data-cy='savLocal'>{location}</p>
+      </div>
     </button>
   )
 }
