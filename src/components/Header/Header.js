fa325194181c107c13559b4c01effaa121299204
Connor Anderson Larson <Connorandersonlarson@gmail.com>
2021-05-24 15:19:10 -0500

    Add styling for day/night mode button

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,7 +8,7 @@ function Header( {hidden} ) {
 
   return (
     <section className='headerLayout' style={color.blue} data-cy='headerLayout'>
-      <button onClick={e => handleModeChange(e)} className='mode' data-cy='mode'>mode</button>
+      <button onClick={e => handleModeChange(e)} className='mode' style={color.orange} data-cy='mode'>&#9788;</button>
       <article className='titleBox' data-cy='titleBox'>
         <h1 className='appTitle' data-cy='appTitle'>Job Finder!</h1>
       </article>
