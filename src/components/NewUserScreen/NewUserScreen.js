51f1be1a16b57a9eedb2773904d848c961c62163
Aaron Foucheaux <afoucheaux@gmail.com>
2021-05-26 20:06:10 -0500

    Travis bug

--- src/components/NewUserScreen/NewUserScreen.js
+++ src/components/NewUserScreen/NewUserScreen.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import NewUserForm from '../NewUserForm/NewUserForm.js';
 import './NewUserScreen.css';
 import Header from '../Header/Header.js';
